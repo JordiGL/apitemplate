66dd5cbf945e91a1c8fe6403e6602d4b4b9ec5f3
JordiGL <jordi.g.l@live.com>
2024-05-10 16:15:32 +0200

    Initial

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.20" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    ktlint {
+        verbose.set(true)
+        android.set(true)
+        filter {
+            exclude("**/generated/**")
+        }
+    }
+}
